e2e847dd47b4a2413413e913aac285c2e7692188
Marta Fondevila <martafondevila8@gmail.com>
2020-04-29 00:17:32 +0200

    Prueba 2

--- src/app/ArchivoTexto.java
+++ src/app/ArchivoTexto.java
@@ -5,17 +5,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArchivoTexto {
-    public static boolean guardarDato(ArrayList<Juego> col, String archivo){
+    public static void guardarDato(ArrayList<Juego> col, String archivo){
+        boolean correcto=true;
         try(PrintWriter f = new PrintWriter(new FileWriter(archivo, false))){
             for(Juego juego: col){
+                correcto=true;
                 f.println(juego.getTitulo());
                 f.println(juego.getAno());
                 f.println(juego.getFabricante());
             }
         }catch(Exception e){
-            return false;
+            correcto=false;
+            System.out.println("Error al guardar los datos: "+e.getMessage());
         }
-        return true;
+        if(correcto){
+            System.out.println("Datos guardados");
+        }
     }
 
     public static void leerDatos(ArrayList<Juego> col, String archivo){
@@ -23,8 +28,10 @@ public class ArchivoTexto {
         int ano;
         String fabricante;
         Juego juego;
+        boolean correcto=true;
         try(Scanner f = new Scanner(new File(archivo))){
             while(f.hasNext()){
+                correcto=true;
                 titulo=f.nextLine();
                 ano=f.nextInt();
                 f.nextLine();
@@ -33,7 +40,11 @@ public class ArchivoTexto {
                 col.add(juego);
             }
         }catch(Exception e){
-            System.out.println("Error: "+e.getMessage());
+            correcto=false;
+            System.out.println("Error al cargar los datos: "+e.getMessage());
+        }
+        if(correcto){
+            System.out.println("Datos cargados");
         }
     }
 }
